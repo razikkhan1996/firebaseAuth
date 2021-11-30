194a5c2fa7cd7041104ef1cf48af066e946974dc
razikkhan1996 <razik99815@gmail.com>
2021-11-30 10:31:08 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBLQxDi3SSiOnNBywm-lQabBkFEglfbP6A",
+    authDomain: "authlogin-245ec.firebaseapp.com",
+    projectId: "authlogin-245ec",
+    storageBucket: "authlogin-245ec.appspot.com",
+    messagingSenderId: "1049676082840",
+    appId: "1:1049676082840:web:03d2560c7d9bac19ccaaed",
+    measurementId: "G-8CLN961K4T"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+  
